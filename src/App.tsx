31ab14cdb088b85f58b06ea7e4eddbcaaa0dd1bd
shannon petkau <spetkau2@rrc.ca>
2026-01-25 11:38:51 -0600

    Added imports to add Routes to App function and created layout page that renders the footer and header of the page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
 import './App.css'
 
-import { Header } from './components/common/Header'
-import { Footer } from './components/common/Footer'
+import { Header } from './components/common/layout/header/Header'
+import { Footer } from './components/common/layout/footer/Footer'
 
 import "./components/reviews/Reviews"
 import Reviews from './components/reviews/Reviews'
@@ -13,13 +16,13 @@ import { PopularGames } from './components/popular_games/PopularGames'
   
 function App() {
   return (
-    <>
+    <Routes>
       <Header />
       <PopularGames />
       <FriendsList />
       <Reviews />
       <Footer />
-    </>
+    </Routes>
   )
 }
 

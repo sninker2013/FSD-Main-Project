295586b277d33f8e7a601b45f8c72e0eef455fee
shannon petkau <spetkau2@rrc.ca>
2026-01-26 11:18:41 -0600

    Added function AllFriends that should return a list of friends after I implement that feature.

---
+++ src/components/pages/friends-page.tsx
@@ -0,0 +1,20 @@
+import type { Friends } from "../../types/friends";
+
+export function AllFriends(
+    {
+        friends
+    }:
+    {
+        friends: Friends[]
+    }
+) {
+    return(
+        <>
+        <header>
+            <h1>All Friends</h1>
+        </header>
+        <main>
+        </main>
+        </>
+    )
+}

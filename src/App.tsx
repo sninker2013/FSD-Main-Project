b1f8fadee0dec0b54f171e7104c1b6cc3744b712
D Synkiw <dsynkiw@rrc.ca>
2026-01-30 14:25:04 -0600

    removed some old imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import { useState } from "react";
 
 import './App.css'
 
 import { Layout } from "./components/common/layout/layout";
-
-import "./components/reviews/Reviews"
 import Reviews from './components/reviews/Reviews'
 
-import "./components/friends_list/friends_list"
-import FriendsList from './components/friends_list/friends_list'
 
-import { PopularGames } from './components/popular_games/PopularGames'
-  
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="/Reviews" element={<Reviews />} />
+      </Route>
     </Routes>
   )
 }
--- src/components/common/layout/layout.tsx
+++ src/components/common/layout/layout.tsx
@@ -1,18 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./header/Header";
 import { Footer } from "./footer/Footer";
-import FriendsList from "../../friends_list/friends_list";
-import { PopularGames } from "../../popular_games/PopularGames";
-import Reviews from "../../reviews/Reviews";
 
 export function Layout() {
     return (
         <>
             <Header />
             <Outlet />
-            <PopularGames />
-            <FriendsList />
-            <Reviews />
             <Footer />
         </>
     )

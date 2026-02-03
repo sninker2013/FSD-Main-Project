14fedea079e44991519a47a031946047c259bbb4
Xavier Balzer <xbalzer@rrc.com>
2026-02-02 18:09:11 -0600

    feat: Integrate UserProfilePage with routing and state management

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,17 @@ import { Layout } from "./components/common/layout/layout";
 import FriendsList from "./components/friends_list/friends_list";
 import { PopularGames } from "./components/popular_games/PopularGames";
 import Reviews from './components/reviews/Reviews'
+import { UserProfilePage } from "./components/pages/UserProfilePage";
+
+import type { UserProfileType, Game } from "./components/common/user-profile/profileData";
+import { testProfile, testGames } from "./components/common/user-profile/profileData";
 
 
 function App() {
   const [status, updateStatus] = useState<string>("");
+  const [profile, setProfile] = useState<UserProfileType>(testProfile);
+  const [games, setGames] = useState<Game[]>(testGames);
+  const [newGameTitle, setNewGameTitle] = useState("");
   
   return (
     <Routes>
@@ -27,6 +34,16 @@ function App() {
         <Route path="PopularGames" element={<PopularGames />}/>
         <Route path="Friends" element={<FriendsList />}/>
         <Route path="Reviews" element={<Reviews />}/>
+        <Route 
+          path="Profile" 
+          element={<UserProfilePage 
+            profile={profile}
+            setProfile={setProfile}
+            games={games}
+            updateGames={setGames}
+            newGameTitle={newGameTitle}
+            setNewGameTitle={setNewGameTitle}
+          />}/>
       </Route>
     </Routes>
   )

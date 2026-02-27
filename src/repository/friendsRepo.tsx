cdc0ffe8ffcd261b36aa0052552274031e7175ee
shannon petkau <spetkau2@rrc.ca>
2026-02-27 08:01:44 -0600

    Added update and delete functions to the friendRepo.

--- src/repository/friendsRepo.tsx
+++ src/repository/friendsRepo.tsx
@@ -17,8 +17,25 @@ export const addFriend = (userName: string): Friends[] => {
         isFavourite: false
     };
 
-    const friends = placeholder();
+    const friends = testFriends();
     friends.push(newFriend);
-    placeholder(friends);
+    testFriends(friends);
     return newFriend;
 };
+
+export const updateFriendFavourite = (friendId: string): void => {
+    const friends = testFriends();
+    const updatedFriends = friends.map((friend) =>
+        friend.id === friendId
+            ? { ...friend, isFavourite: !friend.isFavourite }
+            : friend
+    );
+
+    testFriends(updatedFriends);
+};
+
+export const deleteFriend = (friendId: string): void => {
+    const friends = testFriends();
+    const updatedFriends = friends.filter((friend) => friend.id !== friendId);
+    testFriends(updatedFriends);
+}

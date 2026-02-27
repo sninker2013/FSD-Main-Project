717322bf894ed78a50b0e0cd4f6704dfb3dabf90
shannon petkau <spetkau2@rrc.ca>
2026-02-27 16:10:01 -0600

    Added more test data for friends.

--- src/data/friendsData.tsx
+++ src/data/friendsData.tsx
@@ -1,5 +1,14 @@
-const testFriends: Friends[] = [
+import type { Friends } from "../types/friends";
+
+export const testFriends: Friends[] = [
     {id: "101", userName: "timdrake", isFavourite: false},
     {id: "102", userName: "donnatroy", isFavourite: false},
-    {id: "103", userName: "peterparker", isFavourite: false}
+    {id: "103", userName: "peterparker", isFavourite: false},
+    {id: "104", userName: "mjwatson", isFavourite: false},
+    {id: "105", userName: "surlycat", isFavourite: false},
+    {id: "106", userName: "bearygood", isFavourite: false},
+    {id: "107", userName: "hyperman", isFavourite: false},
+    {id: "108", userName: "daddyhoothoot", isFavourite: false},
+    {id: "109", userName: "blitzrodeo", isFavourite: false},
+    {id: "110", userName: "gothchick", isFavourite: false}
 ]

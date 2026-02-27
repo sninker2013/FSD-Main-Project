dac73afaaf1ddd3fee1f673fe905c27c06588027
shannon petkau <spetkau2@rrc.ca>
2026-02-27 15:46:15 -0600

    Created a file for the friends data.

---
+++ src/data/friendsData.tsx
@@ -0,0 +1,5 @@
+const testFriends: Friends[] = [
+    {id: "101", userName: "timdrake", isFavourite: false},
+    {id: "102", userName: "donnatroy", isFavourite: false},
+    {id: "103", userName: "peterparker", isFavourite: false}
+]

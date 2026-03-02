bc4990d1e54744d57e9f595a7343f1c28a44fe71
Xavier Balzer <xbalzer@rrc.com>
2026-03-02 12:46:42 -0600

    feat: add initial game service structure and documentation

---
+++ src/services/gameService.ts
@@ -0,0 +1,8 @@
+import * as GameRepo from "../repositories/gameRepo";
+import { Game } from "../types/game";
+
+/**
+ * Game Service - Business logic layer for game operations.
+ * Delegates all data access to the GameRepository.
+ */
+

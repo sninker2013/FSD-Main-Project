cbb25634cd3ef663c80eced9fd7c1f563c4caf2e
shannon petkau <spetkau2@rrc.ca>
2026-03-24 08:46:54 -0500

    Created seedData, seed.ts file, and client.ts file.

---
+++ apps/backend/prisma/seed.ts
@@ -0,0 +1,62 @@
+import { PrismaClient } from "@prisma/client";
+import { userSeedData, gameSeedData, friendSeedData, reviewSeedData } from "./seedData";
+
+const prisma = new PrismaClient();
+
+// this method will add default values to the database
+// IT WILL CLEAR THE DB WHEN INVOKED
+// see https://www.prisma.io/docs/orm/prisma-migrate/workflows/seeding
+async function main() {
+    // clear table
+    await prisma.user.deleteMany();
+
+    // insert users to db
+    const createManyUsers = await prisma.user.createManyAndReturn(
+        {
+            data: userSeedData,
+            skipDuplicates: true
+        }
+    );
+
+    await prisma.game.deleteMany();
+
+    // insert games to db
+    const createManyGames = await prisma.game.createManyAndReturn(
+        {
+            data: gameSeedData,
+            skipDuplicates: true
+        }
+    );
+
+    await prisma.friend.deleteMany();
+
+    // insert friends to db
+    const createManyFriends = await prisma.friend.createManyAndReturn(
+        {
+            data: friendSeedData,
+            skipDuplicates: true
+        }
+    );
+
+    await prisma.review.deleteMany();
+
+    // insert reviews to db
+    const createManyReviews = await prisma.review.createManyAndReturn(
+        {
+            data: reviewSeedData,
+            skipDuplicates: true
+        }
+    );
+
+    console.log(`CREATED USERS: ${createManyUsers}, CREATED GAMES: ${createManyGames}, CREATED FRIENDS: ${createManyFriends}, CREATED REVIEWS: ${createManyReviews}`);
+};
+
+main().then(
+    async() => {
+        await prisma.$disconnect()
+    }
+).catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+}); 

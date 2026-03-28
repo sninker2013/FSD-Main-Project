0dd0a91a365812c60158baf92c8968754b973712
shannon petkau <spetkau2@rrc.ca>
2026-03-28 17:16:07 -0500

    Created userSchema.

--- apps/backend/src/api/v1/validations/friendValidation.ts
+++ apps/backend/src/api/v1/validations/friendValidation.ts
@@ -1,7 +1,7 @@
 import Joi, { ObjectSchema } from "joi";
 
-// define the correct shape of a term object received in JSON
-// Require a title and definition string at minimum
+// define the correct shape of a friend object received in JSON
+// Require a userId and friendId string at minimum
 export const friendSchema: ObjectSchema = Joi.object({
     userId: Joi.string().required().messages({
         "any.required": "User ID is required",
@@ -11,9 +11,9 @@ export const friendSchema: ObjectSchema = Joi.object({
         "any.required": "Friend ID is required",
         "string.empty": "Friend ID cannot be empty"
     }),
-    dateAdded: Joi.string().required().messages({
+    dateAdded: Joi.date().required().messages({
         "any.required": "Date Added is required",
-        "string.empty": "Date Added cannot be empty"
+        "date.empty": "Date Added cannot be empty"
     }),
     isFavourite: Joi.boolean().optional()
 });

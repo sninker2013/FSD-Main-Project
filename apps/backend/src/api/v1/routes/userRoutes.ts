6f8e2915352d3b040b7927fc3665ee640e1219ab
shannon petkau <spetkau2@rrc.ca>
2026-03-29 14:33:04 -0500

    Fixed up friendsRoutes and userRoutes.

--- apps/backend/src/api/v1/routes/userRoutes.ts
+++ apps/backend/src/api/v1/routes/userRoutes.ts
@@ -8,8 +8,8 @@ const router: Router = express.Router();
 router.get("/users", /*validateRequest(userSchema),*/
     userController.getAllUsers);
 
-    router.get("/users/:id", /*validateRequest(userSchema),*/
-        userController.getUserById);
+    router.get("/users/:name", /*validateRequest(userSchema),*/
+        userController.getUserByUserName);
 
 router.post("/users", /*validateRequest(userSchema),*/ 
     userController.createUser);

4e117fdbc63d286e353ee43f9a4671a756aa58c9
Xavier Balzer <xbalzer@rrc.com>
2026-03-30 20:57:44 -0500

    refactor CORS configuration to ensure frontend URL is properly formatted

--- apps/backend/config/cors.ts
+++ apps/backend/config/cors.ts
@@ -4,11 +4,13 @@ import { CorsOptions } from "cors";
 const corsOptions: CorsOptions = {
     // throw an error if the request does not come from the list of allowed origins
     origin: function(origin, callback) {
-        const allowedOrigins = [process.env.FRONTEND_URL];
+        process.env.FRONTEND_URL = process.env.FRONTEND_URL || '';
+        const frontendUrl = process.env.FRONTEND_URL.replace(/\/$/, '');
+        const allowedOrigins = [frontendUrl];
 
         // invoke callback (eg. next middleware) if  origin matches or no origin
         // some services (like postman) do not include an origin in their request
-        if(allowedOrigins.includes(origin) || !origin) {
+        if(!origin || allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
             callback(new Error("Not allowed by CORS restriction"), false);

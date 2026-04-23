1c113475b8a7b8f28ab354034e3d1f81d293db35
shannon petkau <spetkau2@rrc.ca>
2026-04-22 19:35:58 -0500

    Took out user-login pages and create-user pages.

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -18,10 +18,6 @@ import { useReviews } from "./hooks/useReviews";
 
 import { SearchResult } from "./components/pages/search-results";
 
-import LoginOptions from "./components/pages/users/login-options-page";
-import LoginPage from "./components/pages/users/login-page";
-import CreateUserPage from "./components/pages/users/create-user-page";
-
 function App() {
 
   const [profile, setProfile] = useState<UserProfileType>(testProfile);
@@ -52,9 +48,6 @@ function App() {
             newGameTitle={newGameTitle}
             setNewGameTitle={setNewGameTitle}
           />}/>
-          <Route path="/login-options" element={<LoginOptions />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/create-user" element={<CreateUserPage />} />
           {// new path to handle searching for games
           }<Route path="games/search" element={<SearchResult/>}/>
       </Route>

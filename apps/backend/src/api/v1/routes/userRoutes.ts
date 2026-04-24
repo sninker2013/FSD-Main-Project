340a0a141127639e42ce132d87985a37082ad934
Xavier Balzer <xbalzer@rrc.com>
2026-04-24 16:06:34 -0500

    add findOrCreateUser middleware to current user routes

--- apps/backend/src/api/v1/routes/userRoutes.ts
+++ apps/backend/src/api/v1/routes/userRoutes.ts
@@ -1,13 +1,16 @@
 import express, { Router } from 'express';
 import { validateRequest } from "../middleware/validate"
 import { userSchema } from "../validations/userValidation"
+import { findOrCreateUser } from '../middleware/findOrCreateUser';
 import * as userController from '../controllers/userController';
 
 const router: Router = express.Router();
 
 router.get('/', userController.getAllUsers);
+router.get('/current', findOrCreateUser, userController.getCurrentUser);
 router.get('/:userName', userController.getUserByUserName);
 router.post('/', validateRequest(userSchema), userController.createUser);
+router.put('/current/status', findOrCreateUser, userController.updateCurrentUserStatus);
 router.delete('/:id', userController.deleteUser);
 
 export default router;
